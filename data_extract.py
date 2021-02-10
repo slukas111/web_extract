ceeabd2499cad01c8dcaea184b1c08301c96c527
Sasha Lukas <sashamati@gmail.com>
2021-02-10 07:22:53 -0800

    from the url etracted xml/html of website, import and installed beautifulsoup4 and lxml

--- data_extract.py
+++ data_extract.py
@@ -1,5 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/IPhone'
 result = requests.get(url).text 
-print(result)
+# print(result)
+
+soup = BeautifulSoup(result, 'lxml')
+table = soup.find('table', class_ = 'wikitable')
+#get all the rows, exccept the first one
+rows = table.find_all('tr')[1::1]
+print(rows)
