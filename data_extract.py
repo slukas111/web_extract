08d1fe760c9f57411a18cf98f2ce974c13005732
Sasha Lukas <sashamati@gmail.com>
2021-02-10 08:51:26 -0800

    open file and write a new csv file with version and price

--- data_extract.py
+++ data_extract.py
@@ -3,18 +3,39 @@ from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/IPhone'
 result = requests.get(url).text 
-# print(result)
+
+phone_dict = {}
 
 soup = BeautifulSoup(result, 'lxml')
 table = soup.find('table', class_ = 'wikitable')
 #get all the rows, exccept the first one
 rows = table.find_all('tr')[1::1]
-print(rows)
+
 
 for row in rows:
     data = row.find_all(['th', 'td'])
     try:
+        #grab a tag, split on the last, get name of phone version
+        iphone_version = (data[0].a.text.split('/')[-1])
+        iphone_version = ''.join(i for i in iphone_version if i.isdigit())
 
-        print(data[0].a.text)
+        price_text = data[8]
+        price = price_text.text.split('/')[-1].split('*')[-1].replace('$','').replace('\n','')
+        #cleaning data and removing unfinished input. store variable of interger.
+        price = int(price)
+        version = int(iphone_version)
+        if version < 2 and price < 1:
+            continue
+        #dictionary of iphones
+        phone_dict[version] = price
     except:
         pass
+# print(phone_dict)
+
+csv = open('iphone_price', 'a')
+csv.write('version, price\n')
+
+for key in phone_dict:
+    csv.write(f"{key}, {phone_dict[key]}\n")
+
+csv.close()
