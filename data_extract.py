3b29c4b5aa9d1bcb92b97efdb52c244292eee475
Sasha Lukas <sashamati@gmail.com>
2021-02-10 10:58:06 -0800

    added new file for matplot to visualize data. can update data in csv file and compute to plot line

--- data_extract.py
+++ data_extract.py
@@ -1,41 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://en.wikipedia.org/wiki/IPhone'
-result = requests.get(url).text 
+url = "https://en.wikipedia.org/wiki/IPhone"
+result = requests.get(url).text
 
 phone_dict = {}
 
-soup = BeautifulSoup(result, 'lxml')
-table = soup.find('table', class_ = 'wikitable')
-#get all the rows, exccept the first one
-rows = table.find_all('tr')[1::1]
+soup = BeautifulSoup(result, "lxml")
+table = soup.find("table", class_="wikitable")
+# get all the rows, exccept the first one
+rows = table.find_all("tr")[1::1]
 
 
 for row in rows:
-    data = row.find_all(['th', 'td'])
+    data = row.find_all(["th", "td"])
     try:
-        #grab a tag, split on the last, get name of phone version
-        iphone_version = (data[0].a.text.split('/')[-1])
-        iphone_version = ''.join(i for i in iphone_version if i.isdigit())
+        # grab a tag, split on the last, get name of phone version
+        iphone_version = data[0].a.text.split("/")[-1]
+        iphone_version = "".join(i for i in iphone_version if i.isdigit())
 
         price_text = data[8]
-        price = price_text.text.split('/')[-1].split('*')[-1].replace('$','').replace('\n','')
-        #cleaning data and removing unfinished input. store variable of interger.
+        price = (
+            price_text.text.split("/")[-1]
+            .split("*")[-1]
+            .replace("$", "")
+            .replace("\n", "")
+        )
+        # cleaning data and removing unfinished input. store variable of interger.
         price = int(price)
         version = int(iphone_version)
         if version < 2 and price < 1:
             continue
-        #dictionary of iphones
-        phone_dict[version] = price
+        # dictionary of iphones
+        phone_dict[version] = int(price)
     except:
         pass
-# print(phone_dict)
 
-csv = open('iphone_price.csv', 'a')
-csv.write('version, price\n')
+
+csv = open("iphone_price.csv", "a")
+csv.write("version,price\n")
 
 for key in phone_dict:
-    csv.write(f"{key}, {phone_dict[key]}\n")
+    csv.write(f"{key},{phone_dict[key]}\n")
 
 csv.close()
--- visual_data.py
+++ visual_data.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('iphone_price.csv')
-print(data['version'])
+data = pd.read_csv("iphone_price.csv")
+print(data['price'])
+
+plt.scatter(data['version'], data['price'])
+plt.show()
