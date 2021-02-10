669a6ee8d2e5dc22b6949f2e31408bc6cf1ac7b2
Sasha Lukas <sashamati@gmail.com>
2021-02-10 07:37:30 -0800

    fixed the error in extraction, Getting iphone versions

--- data_extract.py
+++ data_extract.py
@@ -10,3 +10,11 @@ table = soup.find('table', class_ = 'wikitable')
 #get all the rows, exccept the first one
 rows = table.find_all('tr')[1::1]
 print(rows)
+
+for row in rows:
+    data = row.find_all(['th', 'td'])
+    try:
+
+        print(data[0].a.text)
+    except:
+        pass
